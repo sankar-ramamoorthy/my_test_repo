70f78bd0c8f9a30058d4e587d1daa7a471d9837d
sankar-ramamoorthy <43016553+sankar-ramamoorthy@users.noreply.github.com>
2026-02-17 21:51:55 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,13 +1,1 @@
-# my_test_repository.py
 
-class MyTestRepository:
-    def __init__(self):
-        self.description = "This is a simple repository used for testing the ingestion and processing flow."
-
-    def display_info(self):
-        print("My Test Repository")
-        print(self.description)
-
-if __name__ == "__main__":
-    repo = MyTestRepository()
-    repo.display_info()
